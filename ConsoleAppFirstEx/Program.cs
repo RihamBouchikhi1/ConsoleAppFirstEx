8ef81229dddeaf31b9df84bfb6932d0f214f6c6d
RihamBouchikhi1 <riham.bouchikhi@usmba.ac.ma>
2022-10-28 12:22:44 +0200

    modify

--- ConsoleAppFirstEx/Program.cs
+++ ConsoleAppFirstEx/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,37 @@ namespace ConsoleAppFirstEx
     {
         static void Main(string[] args)
         {
+            int Nb;
+            Console.WriteLine("entrez un nombre entier");
+            Nb = int.Parse(Console.ReadLine());
+            Estpremier(Nb);
+
+
+        }  
+        
+        public static void Estpremier(int N)
+        {
+            int drap = 1;
+            int sqrt_int = (int)Math.Sqrt(N);
+
+            for (int i = 2; i <= sqrt_int; i++)
+            {
+                if(N % i == 0)
+                {
+                    drap = 0;
+                    break;
+                }
+                
+            }
+
+            if (drap == 0)
+                Console.WriteLine("le nombre n'est pas premier");
+            else
+                Console.WriteLine("le nombre est premier");
+
+
+
         }
+
     }
 }
